2aa3573016f5d93f1fccf35002d70eb8ce924567
Ergun Malay <prokillerhd32@gmail.com>
2022-04-18 15:40:09 +0100

    Added a generated welcome message.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
 
+const generateImage = require("./generateimage")
+
 dotenv.config();
 
 const TOKEN = (process.env.DISCORD_BOT_TOKEN);
@@ -8,7 +10,8 @@ const TOKEN = (process.env.DISCORD_BOT_TOKEN);
 const client = new Discord.Client({
     intents:[
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -20,6 +23,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello world")
     }
+})
+
+const welcomeChannelID = "964952236822233178"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelID).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(TOKEN);
